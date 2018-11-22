19526acdd8876e8c97e414cfb7c95a9ba06436db
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-23 00:02:55 +0800

    react-loadable使用

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Route,BrowserRouter} from 'react-router-dom'
 import {Globalstyle} from './style'
 import Header from './common/header/index'
 import Home from './pages/home'
-import Detail from './pages/detail'
+import Detail from './pages/detail/loadable'
 import Login from './pages/login'
 import Write from './pages/write'
 import {Globalstyleicon} from './static/iconfont/iconfont'
@@ -13,25 +13,25 @@ import {Provider} from 'react-redux'
 
 
 class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-          <BrowserRouter>
-            <div>
-                <Globalstyle/>
-                <Globalstyleicon/>
-                <Header/>
-                  <div>
-                    <Route path='/'  exact component={Home}/>
-                    <Route path='/detail/:id' exact component={Detail}/>
-                      <Route path='/login'  exact component={Login}/>
-                      <Route path='/write' component={Write}/>
-                  </div>
-            </div>
-      </BrowserRouter>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <Globalstyle/>
+                        <Globalstyleicon/>
+                        <Header/>
+                        <div>
+                            <Route path='/'  exact component={Home}/>
+                            <Route path='/detail/:id' exact component={Detail}/>
+                            <Route path='/login'  exact component={Login}/>
+                            <Route path='/write' component={Write}/>
+                        </div>
+                    </div>
+                </BrowserRouter>
+            </Provider>
+        );
+    }
 }
 
 export default App;
